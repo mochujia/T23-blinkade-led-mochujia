baccb8d0bc20df46bf36380fa9039d998e2a68ba
mochujia <142893186+mochujia@users.noreply.github.com>
2024-09-18 10:43:42 +0200

    Fick koden att fungera utan for loop och tog bort å och ö

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@
 
 // put function declarations here:
 int myFunction(int, int);
-void blinkande_led_lamp_funktion(int antal_gånger_loop_har_körts);
+void blinkande_led_lamp_funktion(int antal_ganger_loop_har_korts);
 
 #define LED2 2 // Definierar att pin 2 ska kallas LED
 #define LED3 3
@@ -19,26 +19,26 @@ void setup() {
   pinMode(LED5, OUTPUT);
   
 }
-int antal_gånger_loop_har_körts = 0;
+int antal_ganger_loop_har_korts = 0;
 const int lista_med_led[4] = {LED2, LED3, LED4, LED5}; //Sätter alla led lampor i en constant array
 
 
 
 
-void blinkande_led_lamp_funktion(int antal_gånger_loop_har_körts) {
+void blinkande_led_lamp_funktion(int antal_ganger_loop_har_korts) {
 
-  if (antal_gånger_loop_har_körts == 0){
+  if (antal_ganger_loop_har_korts == 0){
     digitalWrite(LED5, LOW);
 
   }
   else{
-    digitalWrite(lista_med_led[antal_gånger_loop_har_körts - 1], LOW); //stänger av den förra lampan
+    digitalWrite(lista_med_led[antal_ganger_loop_har_korts - 1], LOW); //stänger av den förra lampan
 
   }
   
   
 
-  digitalWrite(lista_med_led[antal_gånger_loop_har_körts], HIGH); //sätter på lampan
+  digitalWrite(lista_med_led[antal_ganger_loop_har_korts], HIGH); //sätter på lampan
 
   delay(1000);
   
@@ -57,14 +57,14 @@ void blinkande_led_lamp_funktion(int antal_gånger_loop_har_körts) {
 
 
 void loop() {
-  blinkande_led_lamp_funktion(antal_gånger_loop_har_körts);
+  blinkande_led_lamp_funktion(antal_ganger_loop_har_korts);
 
-  if (antal_gånger_loop_har_körts == 3){
-    antal_gånger_loop_har_körts = 0;
+  if (antal_ganger_loop_har_korts == 3){
+    antal_ganger_loop_har_korts = 0;
   }
 
   else{
-    antal_gånger_loop_har_körts += 1;
+    antal_ganger_loop_har_korts += 1;
 
   }
 
