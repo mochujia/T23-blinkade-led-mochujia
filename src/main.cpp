e91a6a8eda29d7ce2df39cddc8bcfe3f7be414f4
mochujia <142893186+mochujia@users.noreply.github.com>
2024-10-02 09:51:15 +0200

    .

--- src/main.cpp
+++ src/main.cpp
@@ -1,33 +1,18 @@
 
 #include <Arduino.h>
 
-#define LED 2
-#define BUTTON 3
+
 
 
 
 
 
 void setup() {
-  // put your setup code here, to run once:
+  
   Serial.begin(115200);
-  Serial.println("Hello, ESP32-S2!");
-  pinMode(LED, OUTPUT);
-  pinMode(BUTTON, INPUT_PULLUP);
+  
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  if (digitalRead(BUTTON)==LOW){
-    delay(20);
-    if (digitalRead(BUTTON)==LOW){
-      digitalWrite(LED, !digitalRead(LED));
-    }
-  }
-  while (digitalRead(BUTTON) == LOW){
-    delay(20);
-  while (digitalRead(BUTTON) == LOW){
-      delay(20);
-  }
-  }
+  
 }
