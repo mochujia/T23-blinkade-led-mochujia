a0504613a877cc5d4207027ab1bca3e58b23d542
mochujia <142893186+mochujia@users.noreply.github.com>
2024-11-06 09:55:14 +0100

    .

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,26 @@
-
 #include <Arduino.h>
 
-
 #define A0 1
+#define LED 2
+
 
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
   Serial.println("Hello, ESP32-S2!");
-  analogReadResolution(12);
+  pinMode(LED, OUTPUT);
 }
 
 void loop() {
+  unsigned long sensorValue = analogRead(A0) / 2;
+  
+  delay(sensorValue);
+  digitalWrite(LED, HIGH);
+  Serial.println("Lampan tänds");
+  delay(200);
+  digitalWrite(LED, LOW);
 
-  float sensorValue = analogRead(A0);
-  float voltage = sensorValue * (3.3/4095);
-  Serial.println(voltage);
-  // put your main code here, to run repeatedly:
-  delay(10); // this speeds up the simulation
+
+  
 }
